48250c04385e5ad5bbba3794954161f7dac3412b
hbruce48226 <holdenb5@gmail.com>
2019-04-14 17:39:45 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  task: string;
+  complete: boolean;     // -- when true: Complete button vanishes and line gets strikethrough; when false: Complete button nexrt to task 
+  delete: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class TodoComponent {
   title = 'angular-todo-list-lab';
+  hideButton: boolean = false;
+  todoInput: string;
+  todoList: Todo[] = [
+    { task: 'Walk the dog', complete: false, delete: false },
+    { task: 'Get gas', complete: true, delete: false },
+    { task: 'Wash the car', complete: false, delete: false },
+    { task: 'Do all of your homework and study for the exam', complete: false, delete: false }
+  ];
+  isComplete: boolean = false; //initially set complete status to false (incomplete)
+  isDelete: boolean = false;
+
+  addTask = () => { //pressing the Add button calls addTask(), which takes the input value and pushes it to the ul
+    const newTask = {
+      task: this.todoInput, //todoInput is the value of the input bar
+      complete: this.isComplete,
+      delete: this.isDelete
+    };
+    this.todoList.push(newTask);
+    this.todoInput = null; //this clears out the inputs once the submit button has been clicked
+  };
+
+  strikeThru = (i) => {
+    this.todoList[i]['complete'] = !this.todoList[i]['complete']; //changes value of complete to true, making the task complete
+    this.hideButton = !this.hideButton; //changes value of hide to true, hiding the button
+  };
+
+  reverseStrikeThru = (i) => {
+    if (this.todoList[i]['complete'] === true) {
+      this.todoList[i]['complete'] = !this.todoList[i]['complete'];
+      this.hideButton = !this.hideButton;
+    }
+  };
+
+  removeTask = (i) => {
+    this.todoList.splice(i, 1);
+    // this.todoList[i]['delete'] = !this.todoList[i]['delete'];
+    // this.isDelete = !this.isDelete;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { TodoComponent } from './app.component';
 @NgModule({
   declarations: [
-    AppComponent
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [TodoComponent]
 })
 export class AppModule { }
